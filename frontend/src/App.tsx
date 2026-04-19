c9035c32eaf4778719d9a2ec2c5de44f279a062b
Arvin Ferrer <60701881+arvin-ferrer@users.noreply.github.com>
2026-04-19 18:58:27 +0800

    feat: add new route for home, login, and register pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
   return (
     <>
-    <h1>FARM-TO-TABLE</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
